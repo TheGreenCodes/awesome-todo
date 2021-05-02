bcfc5e2a21ba42727a5e814aafd74c3ffeb5355f
MarvinKweyu <mkweyu1@gmail.com>
2021-05-02 20:02:34 +0300

    chore: create home container for tasks and tests for home title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Welcome to TheGreenCodes awesome to-do list</h2>
+    <div class="container">
+      <b-card title="Card title" sub-title="Card subtitle" class="list-container">
+
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import ListContainer from '@/components/ListContainer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // ListContainer
   }
 }
 </script>
+
+<style scoped>
+.list-container{
+    max-width: 170%;
+}
+</style>
