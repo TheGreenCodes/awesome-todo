b994647a4b4001863d0ddad9ebe8b01f3d9d391c
MarvinKweyu <mkweyu1@gmail.com>
2021-05-03 19:43:39 +0300

    chore: create basic project layout

--- src/views/Completed.vue
+++ src/views/Completed.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="completed">
+    <h2>This is the Completed page</h2>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h2>Welcome to TheGreenCodes awesome to-do list</h2>
     <div class="container">
-      <b-card title="Card title" sub-title="Card subtitle" class="list-container">
+      <b-card class="list-container">
 
       </b-card>
     </div>
@@ -10,14 +10,8 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import ListContainer from '@/components/ListContainer.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    // ListContainer
-  }
+  name: 'Home'
 }
 </script>
 
